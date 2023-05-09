b81a32668db8d543e256c3cb6c7affa56ced8c8a
MuhRia1 <132400031+MuhRia1@users.noreply.github.com>
2023-05-09 15:13:46 -0500

    Add files via upload

---
+++ funcydoody.py
@@ -0,0 +1,12 @@
+a = '12371746782563845683756239845093457209384572346702983567203956790246'
+b = '1'
+
+converted_a = int(a)
+converted_b = int(b)
+
+sum = converted_a + converted_b
+
+converted_sum = str(sum)
+
+print(type(converted_sum))
+print(converted_sum)
